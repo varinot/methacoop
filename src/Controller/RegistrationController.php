79d53df6d55a15ae9425fb283f267e6e630644ea
Philippe <varinotformation88@gmail.com>
2021-02-11 17:06:23 +0100

    un peu de front pour utiliser une barre de message

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -32,6 +32,12 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginformAuthenticator $authenticator, ORMEntityManagerInterface $em): Response
     {
+        // si déjà connecté on redirige vers accueil quand la vérif de connection
+       // if (this->getUser()){
+         //   this->addFlash('erreur', 'vous êtes déjà connecté');
+
+           // return $this->redirectToRoute('accueil);
+        //  }
         $user = new User;
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -50,13 +56,13 @@ class RegistrationController extends AbstractController
             $em->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('methacoop@methacoop.com', 'methacoop'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+           // $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+             //   (new TemplatedEmail())
+             //       ->from(new Address('noreply@methacoop.com', 'methacoop'))
+             //       ->to($user->getEmail())
+             //       ->subject('Confirmez votre Email')
+             //       ->htmlTemplate('registration/confirmation_email.html.twig')
+            // );
             // do anything else you need here, like send an email
 
             return $guardHandler->authenticateUserAndHandleSuccess(
@@ -89,8 +95,8 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
-
-        return $this->redirectToRoute('app_register');
+        $this->addFlash('OK', 'votre email est vérifié.');
+// redirection vers page accueil si email vérifié 
+        return $this->redirectToRoute('accueil');
     }
 }
