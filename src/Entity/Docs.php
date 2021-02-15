7a1d5bf1762839ef613a48cf1b01503edb051425
Philippe <varinotformation88@gmail.com>
2021-02-15 11:28:56 +0100

    modif du vich_uploader concernant les docs

--- src/Entity/Docs.php
+++ src/Entity/Docs.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\DocsRepository;
-
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=DocsRepository::class)
+ * @Vich\Uploadable
  * @ORM\HasLifecycleCallbacks
  */
 class Docs
@@ -22,6 +24,15 @@ class Docs
      * @ORM\Column(type="string", length=110)
      */
     private $doctit;
+
+     /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="docs_image", fileNameProperty="docref")
+     * 
+     * @var File|null
+     */
+    private $imageFile;
 
     /**
      * @ORM\Column(type="string", length=40)
@@ -43,11 +54,36 @@ class Docs
         return $this->doctit;
     }
 
-    public function setDoctit(string $doctit): self
+    public function setDoctit(?string $doctit): self
     {
         $this->doctit = $doctit;
 
         return $this;
+    }
+
+     /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 
     public function getDocref(): ?string
@@ -55,7 +91,7 @@ class Docs
         return $this->docref;
     }
 
-    public function setDocref(string $docref): self
+    public function setDocref(?string $docref): self
     {
         $this->docref = $docref;
 
