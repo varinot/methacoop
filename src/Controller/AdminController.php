daeed697a0ed252ad5912c79858081f91ada0c11
Philippe <varinotformation88@gmail.com>
2021-02-16 11:49:00 +0100

    modif des templates de gestion admin et ajout fixtures user modif du controller admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\News;
+use App\Entity\Docs;
+use App\Entity\User;
 use App\Repository\DocsRepository;
 use App\Repository\NewsRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,23 +28,54 @@ class AdminController extends AbstractController
      * @Route("/admin/gesactus", name="app_admin_gesactus")
      */
     public function gesactus(NewsRepository $newsRepository): Response
-    { $news = $newsRepository->findBy([], ['createdAt' => 'DESC']);
+    { 
+        $news = $newsRepository->findBy([], ['createdAt' => 'DESC']);
         return $this->render('admin/gesactus_index.html.twig', compact('news'));
     }
+
+    /**
+     * @Route("/admin/gesactus_detail/{id}", name="app_admin_gesactus_detail")
+     */
+    public function actudetail(News $new): Response
+    {  
+                   
+        return $this->render('admin/gesactus_detail.html.twig', compact('new'));
+    }
+
     /**
      * @Route("/admin/gesdocs", name="app_admin_gesdocs")
      */
     public function gesdocs(DocsRepository $docsRepository): Response
-    {$docs = $docsRepository->findBy([], ['createdAt' => 'DESC']);
-        return $this->render('admin/gesdocs_index.html.twig');
+    { 
+        $docs = $docsRepository->findBy([], ['createdAt' => 'DESC']);
+        return $this->render('admin/gesdocs_index.html.twig', compact('docs'));
     }
+    /**
+     * @Route("/admin/gesdocs_detail/{id}", name="app_admin_gesdocs_detail")
+     */
+    public function docdetail(Docs $doc): Response
+    {  
+                   
+        return $this->render('admin/gesdocs_detail.html.twig', compact('doc'));
+    }
+
     /**
      * @Route("/admin/gesusers", name="app_admin_gesusers")
      */
-    public function gesusers(): Response
+    public function gesusers(UserRepository $userRepository): Response
     {
-        return $this->render('admin/gesusers_index.html.twig');
+        $utils = $userRepository->findBy([], ['createdAt' => 'DESC']);
+        return $this->render('admin/gesusers_index.html.twig', compact('utils'));
     }
+/**
+     * @Route("/admin/gesusers_detail/{id}", name="app_admin_gesusers_detail")
+     */
+    public function utildetail(User $util): Response
+    {  
+         dd($util);          
+        return $this->render('admin/gesusers_detail.html.twig', compact('util'));
+    }
+
     /**
      * @Route("/admin/gesdepots", name="app_admin_gesdepots")
      */
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,28 +12,40 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         // creation documentations
-        
+        $doc2 = new Docs();
+        $doc2->setDoctit('documentation 2da');
+        $doc2->setDocref('reference 2da');
+        $manager->persist($doc2);
+        $manager->flush();
 
             $doc1 = new Docs();
             $doc1->setDoctit('documentation 1da');
             $doc1->setDocref('reference 1da');
-        
             $manager->persist($doc1);
-            $manager->flush();              
-         // creation  actualités
+            $manager->flush(); 
+
+            $doc21 = new Docs();
+            $doc21->setDoctit('documentation 421da');
+            $doc21->setDocref('reference 421da');
+            $manager->persist($doc21);
+            $manager->flush(); 
          
- 
+            // creation  actualités
+         
             $actu1 = new News();
             $actu1->setNewstit('actualité 1a');
             $actu1->setNewscont('contenu actu 1a');
-         //    $actu->setCreatedAt = new \DateTime('NOW');
             $manager->persist($actu1);
             $manager->flush(); 
             
             for ($i = 0; $i < 10; $i++) {
                 $actu = new News();
                 $actu->setNewstit('actualité '.$i);
-                $actu->setNewscont('contenu actu  '.$i);
+                $actu->setNewscont('Cela ne sera pas une surprise 
+                 mais nous avons le regret de vous annoncer que BioExpo
+                  ne se déroulera pas dans un format physique à 
+                 la fin du mois de mars comme annoncé et comme nous espérions.
+                Cela dit, nous avons encore des choses en réserve pour vous. '.$i);
                 $manager->persist($actu);
             }
                 $manager->flush();
