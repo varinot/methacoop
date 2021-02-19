dbb43004e44446e79e2b1dde96553e0217c44371
Philippe <varinotformation88@gmail.com>
2021-02-19 11:59:00 +0100

    modif du type de formulaire de documentation pour tenir compte des images et fichiers document

--- src/Form/DocuType.php
+++ src/Form/DocuType.php
@@ -6,16 +6,34 @@ use App\Entity\Docs;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Vich\UploaderBundle\Form\Type\VichImageType; 
 
 class DocuType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('doctit')
-            ->add('docref')
+            ->add('imageFile', VichImageType::class, [
+            'required' => false,
+            'allow_delete' => true,
+            'delete_label' => '...',
+            'download_label' => '...',
+            'download_uri' => true,
+            'image_uri' => true,
             
-        ;
+            'asset_helper' => true,
+            ])
+            ->add('genericFile', VichFileType::class, [
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => '...',
+                'download_uri' => '...',
+                'download_label' => '...',
+                'asset_helper' => true,
+            ])
+            ->add('doctit')
+            ->add('docref');
     }
 
     public function configureOptions(OptionsResolver $resolver)
